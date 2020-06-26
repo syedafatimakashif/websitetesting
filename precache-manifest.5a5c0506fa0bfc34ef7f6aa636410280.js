980ebd6904688339e14cdc1792b091e3f25b1b10
Fatima Kashif <syedafatimakashif09@gmail.com>
2020-06-26 18:36:48 +0500

    Updates

---
+++ precache-manifest.5a5c0506fa0bfc34ef7f6aa636410280.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180d97576a99c985a7347fff871f0d32",
+    "url": "/websitetesting/index.html"
+  },
+  {
+    "revision": "eb9a0a39f648332ceb9d",
+    "url": "/websitetesting/static/css/2.7fbd2046.chunk.css"
+  },
+  {
+    "revision": "a26a1634a87e48946549",
+    "url": "/websitetesting/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "eb9a0a39f648332ceb9d",
+    "url": "/websitetesting/static/js/2.740e1802.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/websitetesting/static/js/2.740e1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26a1634a87e48946549",
+    "url": "/websitetesting/static/js/main.15baf2de.chunk.js"
+  },
+  {
+    "revision": "de450458f3669dcf1267",
+    "url": "/websitetesting/static/js/runtime-main.d2c6a7bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/websitetesting/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/websitetesting/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/websitetesting/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/websitetesting/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/websitetesting/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
